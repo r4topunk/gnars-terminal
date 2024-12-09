797263c458b4ef7e9120b507a76a913b102d408c
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-12-09 15:27:43 -0300

    refactor: remove unused code

--- src/components/utils/zora.tsx
+++ src/components/utils/zora.tsx
@@ -1,31 +1,22 @@
 'use client';
 
 import { useState } from 'react';
-import { Address, Chain } from 'viem';
+import { Address } from 'viem';
 
 export default function ZoraEmbed({
   chainContract,
 }: {
   chainContract: `${string}:${Address}`;
 }) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const isDarkMode = document.documentElement.classList.contains('dark');
-
-  const src = `https://zora.co/collect/${chainContract}/embed?referrer=0x41CB654D1F47913ACAB158a8199191D160DAbe4A`;
-  const iframeSrc = isDarkMode ? `${src}&theme=dark` : src;
-
-  const handleLoad = () => {
-    setIsLoaded(true);
-  };
+  const iframeSrc = `https://zora.co/collect/${chainContract}/embed?referrer=0x41CB654D1F47913ACAB158a8199191D160DAbe4A`;
 
   return (
     <iframe
       src={iframeSrc}
-      onLoad={handleLoad}
+      style={{ borderRadius: '12px' }}
       width='100%'
       height='100%'
       title='Zora iFrame'
-      style={isLoaded ? {} : { display: 'none' }}
     />
   );
 }
